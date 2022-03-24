81faf13d95ba8b5fbfca8bd89ae1caceed5daeb6
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-25 02:52:16 +0300

    add pack module

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
 """This script will distribute archive in webserver"""
 
-from fabric.api import run, put, env
-from os.path import exists
+from datetime import datetime
+from fabric.api import run, put, env, local
+from os.path import exists, isdir
 
 env.hosts = [
     '35.227.37.173',
     '18.207.135.118'
 ]
 env.user = 'ubuntu'
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir('versions') is False:
+            local('mkdir versions')
+        arch_path = "versions/web_static_{}.tgz".format(date)
+        local('tar -cvzf {} web_static'.format(arch_path))
+        return arch_path
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -20,15 +34,15 @@ def do_deploy(archive_path):
             file = archive_path.split('/')[1]
             arch = file.split(".")[0]
             put(archive_path, "/tmp/")
-            run("sudo mkdir -p /data/web_static/releases/{}/".format(arch))
-            run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            run("mkdir -p /data/web_static/releases/{}/".format(arch))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
                 .format(file, arch))
-            run("sudo rm /tmp/{}".format(file))
-            run("sudo mv /data/web_static/releases/{}/web_static/* \
+            run("rm /tmp/{}".format(file))
+            run("mv /data/web_static/releases/{}/web_static/* \
                 /data/web_static/releases/{}/".format(arch, arch))
-            run("sudo rm -rf /data/web_static/releases/{}/web_static/".format(arch))
-            run("sudo rm -rf /data/web_static/current")
-            run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            run("rm -rf /data/web_static/releases/{}/web_static/".format(arch))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
                 .format(arch))
             return True
         except:
