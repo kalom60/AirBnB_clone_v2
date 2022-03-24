dda965f896c6a86307bb2da7706aecc56b9dc208
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-25 01:13:51 +0300

    fix lint

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -30,7 +30,9 @@ exec {'owner':
 }
 
 exec {'content':
-  command  => 'sudo sed -i \'43i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t autoindex on;\n\t}\n\' /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i \'43i\\n\tlocation /hbnb_static/\
+  {\n\t\talias /data/web_static/current/;\n\t\
+  autoindex on;\n\t}\n\' /etc/nginx/sites-available/default',
   provider => shell,
   before   => Exec['config'],
 }
