2e891173ff25adb8ec7462cdfc81a07fcc76b421
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-25 02:30:30 +0300

    add try clause

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,26 +13,23 @@ env.user = 'ubuntu'
 
 def do_deploy(archive_path):
     """depoly archives"""
-    if archive_path:
-        archive_file = archive_path.split('/')[1]
-        archive_dir = archive_file.split('.')[0]
-        releases = '/data/web_static/releases/'
-        current = '/data/web_static/current'
-
+    if exists(archive_path) is False:
+        return False
+    else:
         try:
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}{}'.format(releases, archive_dir))
-            run('tar -xzf /tmp/{} -C {}{}'.format(
-                archive_file, releases, archive_dir))
-            run('rm /tmp/{}'.format(archive_file))
-            run('mv {}{}/web_static/* {}{}'.format(
-                releases, archive_dir, releases, archive_dir))
-            run('rm -rf {}{}/web_static'.format(releases, archive_dir))
-            run('rm -rf {}'.format(current))
-            run('ln -s {}{} {}'.format(releases, archive_dir, current))
-
+            file = archive_path.split('/')[1]
+            arch = file.split(".")[0]
+            put(archive_path, "/tmp/")
+            run("mkdir -p /data/web_static/releases/{}/".format(arch))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+                .format(file, arch))
+            run("rm /tmp/{}".format(file))
+            run("mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(arch, arch))
+            run("rm -rf /data/web_static/releases/{}/web_static/".format(arch))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                .format(arch))
             return True
         except:
             return False
-    else:
-        return False
