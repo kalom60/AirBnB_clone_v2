4307989fbf7f04aced822f88117a9a5dfa3e4618
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-24 22:51:51 +0300

    fix errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,21 +16,23 @@ env.user = 'ubuntu'
 
 def do_deploy(archive_path):
     """send archive to web server"""
-    if exists(archive_path) is False:
-        return False
-
-    try:
-        file = archive_path.split('/')[1]
-        file_name = file.split('.')[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file, path, file_name))
-        run('rm -rf /tmp/{}'.format(file))
-        run('mv {0}{1}/web_static/* {0}{1}'.format(path, file_name))
-        run('rm -rf {}{}/web_static'.format(path, file_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_name))
-        return True
-    except:
+    if exists(archive_path):
+        try:
+            file = archive_path.split('/')[1]
+            file_name = file.split(".")[0]
+            path = "{}"
+            put(archive_path, "/tmp/")
+            run("mkdir -p {}{}/".format(path, file_name))
+            run("tar -xzf /tmp/{} -C {}{}/"
+                .format(file, path, file_name))
+            run("rm /tmp/{}".format(file))
+            run("mv {0}{1}/web_static/* \
+                {0}{1}/".format(path, file_name))
+            run("rm -rf {}{}/web_static/".format(path, file_name))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {}{}/ /data/web_static/current"
+                .format(path, file_name))
+        except:
+            return False
+    else:
         return False
