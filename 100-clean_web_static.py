2c1286d153d6e3cf2920c904fc456748372b9c32
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-25 02:02:12 +0300

    fix error

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Function that deploys """
-from fabric.api import *
+from fabric.api import local, run, env
+from os.path import exists
 
 
 env.hosts = ['35.231.33.237', '34.74.155.163']
@@ -10,10 +11,12 @@ env.user = "ubuntu"
 def do_clean(number=0):
     """ remove archive """
 
-    number = int(number)
-
-    number = 1 if int(number) == 0 else int(number)
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    if exists('versions'):
+        if int(number) == 0:
+            num = 2
+        else:
+            num = int(number) + 1
+        cmd = 'tail -n +{}| xargs rm -rf'.format(num)
+        local('cd versions ; ls -t|{}'.format(cmd))
+        path = '/data/web_static/releases'
+        run('cd {}; ls -t| grep web_static|{}'.format(path, cmd))
