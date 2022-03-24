70328f35f404ba08ca766d0ca74112e5b6c304fb
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-25 01:25:25 +0300

    fix error

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,10 +12,7 @@ def do_clean(number=0):
 
     number = int(number)
 
-    if number == 0:
-        number = 2
-    else:
-        number += 1
+    number = 1 if int(number) == 0 else int(number)
 
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
