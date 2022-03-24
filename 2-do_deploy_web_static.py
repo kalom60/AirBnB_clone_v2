572267d38ed9cd2a5164c133b2eb70b94025e9aa
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-24 23:45:50 +0300

    change code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,14 +20,15 @@ def do_deploy(archive_path):
         return False
 
     try:
-        file = archive_path.split('/')[1]
-        file_name = file.split('.')[0]
+        file = archive_path.split("/")[-1]
+        file_name = file.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, file_name))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file, path, file_name))
-        run('rm -rf /tmp/{}'.format(file))
-        run('mv {0}{1}/web_static/* {0}{1}'.format(path, file_name))
+        run('rm /tmp/{}'.format(file))
+        run('mv {}{}/web_static/* {}{}/'
+            .format(path, file_name, path, file_name))
         run('rm -rf {}{}/web_static'.format(path, file_name))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, file_name))
