6b2c4c223bd6f33cfa3908e73dd560d8a2fba0a5
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-25 02:49:45 +0300

    fix error

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,43 +1,29 @@
-# set_up the servers
-
-exec {'install-Nginx':
-  command  => 'sudo apt update ; sudo apt -y install nginx',
-  provider => shell,
-  before   => Exec['create folders'],
+# puppet manifest preparing a server for static content deployment
+exec { 'apt-get-update':
+  command => '/usr/bin/env apt-get -y update',
 }
-
-exec {'create folders':
-  command  => 'sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/',
-  provider => shell,
-  before   => Exec['create file'],
+-> exec {'b':
+  command => '/usr/bin/env apt-get -y install nginx',
 }
-
-exec {'create file':
-  command  => 'echo "Fake content" | sudo tee /data/web_static/releases/test/index.html',
-  provider => shell,
-  before   => Exec['symbolic'],
+-> exec {'c':
+  command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
 }
-
-exec {'symbolic':
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  provider => shell,
-  before   => Exec['owner'],
+-> exec {'d':
+  command => '/usr/bin/env mkdir -p /data/web_static/shared/',
 }
-
-exec {'owner':
-  command => '/bin/chown -R ubuntu:ubuntu /data/',
-  before  => Exec['content'],
+-> exec {'e':
+  command => '/usr/bin/env echo "Puppet x Holberton School" > /data/web_static/releases/test/index.html',
 }
-
-exec {'content':
-  command  => 'sudo sed -i \'43i\\n\tlocation /hbnb_static/\
-  {\n\t\talias /data/web_static/current/;\n\t\
-  autoindex on;\n\t}\n\' /etc/nginx/sites-available/default',
-  provider => shell,
-  before   => Exec['config'],
+-> exec {'f':
+  command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
 }
-
-exec {'config':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+-> exec {'h':
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/ \
+  { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+}
+-> exec {'i':
+  command => '/usr/bin/env service nginx restart',
+}
+-> exec {'g':
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
 }
