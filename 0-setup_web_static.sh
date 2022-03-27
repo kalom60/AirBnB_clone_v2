38c92271e7419a1130cf0d1a174cbbc0ba13f5c6
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-27 21:38:48 +0300

    pep8

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+# Write a Bash script that sets up your web servaers for the deployment of web_static. It must:
+# Install Nginx if it not already installed
+sudo apt-get update
+sudo apt-get -q -y install nginx
+sudo service nginx start
 
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/ 
-echo "<html>
+sudo mkdir -p /data/web_static/{releases/test,shared}
+# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+sudo echo "<html>
     <head>
     </head>
     <body>
         Holberton School
     </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-if [ -L "/data/web_static/current" ];
-then
-    sudo unlink /data/web_static/current
-fi
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -ie '0,/location \/ {/s/location \/ {/location \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;/' /etc/nginx/sites-available/default
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that send an
-archive to the web servers
+Fabric script (based on the file 1-pack_web_static.py) that
+       distributes an archive to your web servers
+Returns False if the file at the path archive_path doesn't exist
 """
-
-from fabric.api import put, run, env
-from os.path import exists
-
-env.hosts = [
-    '35.227.37.173',
-    '18.207.135.118'
-]
-env.user = "ubuntu"
+import os.path
+from fabric.api import *
+from fabric.operations import run, put, sudo
+env.hosts = ['35.227.37.173', '18.207.135.118']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ script that distributes archive to web servers
+    All remote commands must be executed on your both web servers
+    (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+    Returns True if all operations has been done correctly,
+            otherwise returns False
+    """
+    if (os.path.isfile(archive_path) is False):
         return False
+
     try:
-        arch = archive_path.split("/")[-1]
-        f_name = arch.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, f_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(arch, path, f_name))
-        run('rm /tmp/{}'.format(arch))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, f_name))
-        run('rm -rf {}{}/web_static'.format(path, f_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, f_name))
+        """Upload the archive to the /tmp/ directory of the web server"""
+        put(archive_path, "/tmp/")
+        unpack = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + unpack.split(".")[0])
+        run("sudo mkdir -p {:s}".format(folder))
+        run("sudo tar -xzf /tmp/{:s} -C {:s}".format(unpack, folder))
+        run("sudo rm /tmp/{:s}".format(unpack))
+        run("sudo mv {:s}/web_static/* {:s}/".format(folder, folder))
+        run("sudo rm -rf {:s}/web_static".format(folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {:s} /data/web_static/current".format(folder))
         return True
     except:
         return False
