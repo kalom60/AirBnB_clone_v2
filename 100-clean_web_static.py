75b299fcc8c386d0de925b14c15d67085d19e6e1
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-28 00:32:17 +0300

    pep8

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Function that deploys """
 from fabric.api import *
-from os.path import exists
 
 env.hosts = [
     '35.227.37.173',
@@ -12,12 +11,13 @@ env.user = "ubuntu"
 
 def do_clean(number=0):
     """remove archive"""
-    if exists('versions'):
-        if int(number) == 0:
-            num = 1
-        else:
-            num = int(number)
-        command = 'tail -n +{}| xargs rm -rf'.format(num)
-        local('cd versions ; ls -t|{}'.format(command))
-        dir = '/data/web_static/releases'
-        run('cd {}; ls -t| grep web_static|{}'.format(dir, command))
+    number = int(number)
+    if number == 0 or number == 1:
+        local("cd versions && ls -C1 -t| awk 'NR>1'|xargs rm -rf")
+        run("cd /data/web_static/releases && ls -C1 -t| awk \
+            'NR>1'|xargs rm -rf")
+    else:
+        local("cd versions && ls -C1 -t| awk \
+            'NR>{}'|xargs rm -rf".formta(number))
+        run("cd /data/web_static/releases && ls -C1 -t| awk \
+            'NR>{}'|xargs rm -rf".formta(number))
