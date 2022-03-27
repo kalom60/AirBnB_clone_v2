2a213ffd38e08a8e8fa010381966ccf6d385e103
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-27 17:15:21 +0300

    add location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,5 +9,5 @@ sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Hello AirBnB!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -ie '0,/location \/ {/s/location \/ {/location \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;/' /etc/nginx/sites-available/default
 sudo service nginx restart
