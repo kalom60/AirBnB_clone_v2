862ca163fef7d290eaa9f4257f6d84aa792be2b7
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-28 00:20:49 +0300

    change if case

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,11 +13,11 @@ def do_clean(number=0):
     """remove archive"""
     number = int(number)
     if number == 0 or number == 1:
-        local("cd versions && ls -C1 -t| awk 'NR>1'|xargs rm")
+        local("cd versions && ls -C1 -t| awk 'NR>1'|xargs rm -rf")
         run("cd /data/web_static/releases && ls -C1 -t| awk \
-            'NR>1'|xargs rm")
+            'NR>1'|xargs rm -rf")
     else:
         local("cd versions && ls -C1 -t| awk \
-            'NR>{}'|xargs rm".formta(number))
+            'NR>{}'|xargs rm -rf".formta(number))
         run("cd /data/web_static/releases && ls -C1 -t| awk \
-            'NR>{}'|xargs rm".formta(number))
+            'NR>{}'|xargs rm -rf".formta(number))
