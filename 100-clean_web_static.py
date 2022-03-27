b01e0e56a40508f0127c90eda0d47c9d681d92e2
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-27 22:46:26 +0300

    fix codde

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Function that deploys """
-from fabric.api import run, env, local
+from fabric.api import *
+import os
 
 env.hosts = [
     '35.227.37.173',
@@ -10,11 +11,15 @@ env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """remove archive"""
-    if number >= "2":
-        local("cd versions/ && rm $(ls -t | awk 'NR>{}')".format(number))
-        run("cd /data/web_static/releases && rm -rf $(ls -t | awk 'NR>{}')"
-            .format(number))
-    elif number == "1" or number == "0":
-        local("cd versions/ && rm $(ls -t | awk 'NR>1')")
-        run("cd /data/web_static/releases && rm -rf $(ls -t | awk 'NR>1')")
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
