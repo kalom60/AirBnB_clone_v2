efa936353e1687b654e9084eb51cbce73f646b20
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-27 18:12:52 +0300

    add simple html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,20 @@
 # sets up the web servers for the deployment of web_static
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/ 
-echo "Hello AirBnB!" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+if [ -L "/data/web_static/current" ];
+then
+    sudo unlink /data/web_static/current
+fi
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -ie '0,/location \/ {/s/location \/ {/location \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;/' /etc/nginx/sites-available/default
