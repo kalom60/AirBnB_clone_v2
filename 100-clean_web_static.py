0f2a2d8e43ca4b1e8c67b6c2489ff52ebd68cac5
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-28 00:27:33 +0300

    fix error

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Function that deploys """
-from fabric.api import run, env, local
+from fabric.api import *
+from os.path import exists
 
 env.hosts = [
     '35.227.37.173',
@@ -11,13 +12,12 @@ env.user = "ubuntu"
 
 def do_clean(number=0):
     """remove archive"""
-    number = int(number)
-    if number == 0 or number == 1:
-        local("cd versions && ls -C1 -t| awk 'NR>1'|xargs rm -rf")
-        run("cd /data/web_static/releases && ls -C1 -t| awk \
-            'NR>1'|xargs rm -rf")
-    else:
-        local("cd versions && ls -C1 -t| awk \
-            'NR>{}'|xargs rm -rf".formta(number))
-        run("cd /data/web_static/releases && ls -C1 -t| awk \
-            'NR>{}'|xargs rm -rf".formta(number))
+    if exists('versions'):
+        if int(number) == 0:
+            num = 1
+        else:
+            num = int(number)
+        command = 'tail -n +{}| xargs rm -rf'.format(num)
+        local('cd versions ; ls -t|{}'.format(command))
+        dir = '/data/web_static/releases'
+        run('cd {}; ls -t| grep web_static|{}'.format(dir, command))
