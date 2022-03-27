65382d292a66fce0447d7a18c46c9e75c59194e8
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-28 00:12:01 +0300

    change data type

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,12 +11,13 @@ env.user = "ubuntu"
 
 def do_clean(number=0):
     """remove archive"""
-    if int(number) == 0 or int(number) == 1:
-        local("cd versions/ && ls -C1 -t| awk 'NR>1'|xargs rm")
+    number = int(number)
+    if number == 0 or number == 1:
+        local("cd versions && ls -C1 -t| awk 'NR>1'|xargs rm")
         run("cd /data/web_static/releases && ls -C1 -t| awk \
             'NR>1'|xargs rm")
     else:
-        local("cd versions/ && ls -C1 -t| awk \
-            'NR>{}'|xargs rm".formta(int(number)))
+        local("cd versions && ls -C1 -t| awk \
+            'NR>{}'|xargs rm".formta(number))
         run("cd /data/web_static/releases && ls -C1 -t| awk \
-            'NR>{}'|xargs rm".formta(int(number)))
+            'NR>{}'|xargs rm".formta(number))
