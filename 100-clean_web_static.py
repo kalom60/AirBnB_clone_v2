d756da7fa714176f203bb66b528f4088cc317a09
kalom60 <kaleabtesfaye00@gmail.com>
2022-03-27 23:50:31 +0300

    add run

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,12 +12,11 @@ env.user = "ubuntu"
 def do_clean(number=0):
     """remove archive"""
     if int(number) == 0 or int(number) == 1:
-        local('cd versions/')
-        local("ls -C1 -t| awk 'NR>1'|xargs rm")
-        run('cd /data/web_static/releases/')
-        run("ls -C1 -t| awk 'NR>1'|xargs rm")
+        local("cd versions/ && ls -C1 -t| awk 'NR>1'|xargs rm")
+        run("cd /data/web_static/releases && ls -C1 -t| awk \
+            'NR>1'|xargs rm")
     else:
-        local('cd versions/')
-        local("ls -C1 -t| awk 'NR>{}'|xargs rm".formta(int(number)))
-        run('cd /data/web_static/releases/')
-        run("ls -C1 -t| awk 'NR>{}'|xargs rm".formta(int(number)))
+        local("cd versions/ && ls -C1 -t| awk \
+            'NR>{}'|xargs rm".formta(int(number)))
+        run("cd /data/web_static/releases && ls -C1 -t| awk \
+            'NR>{}'|xargs rm".formta(int(number)))
