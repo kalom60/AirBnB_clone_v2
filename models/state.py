518f5d61ff5cf935ba167f1d26695ed1ccff01d4
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-08 00:11:08 +0300

    f your storage engine is not DBStorage, add a public getter method cities to return the list of City objects from storage linked to the current State

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from unittest import result
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -14,17 +16,18 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         cities = relationship("City", cascade="all", backref="state")
+
     else:
         name = ""
 
         @property
         def cities(self):
             """
-            getter attribute cities that returns the list of
-            City instances with state_id equals to the current State.id
+            return the list of City objects from
+            storage linked to the current State
             """
             rets = []
-            for x in models.storage.all(State).values():
-                if x.state_id == self.id:
-                    rets.append(x)
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    rets.append(city)
             return rets
